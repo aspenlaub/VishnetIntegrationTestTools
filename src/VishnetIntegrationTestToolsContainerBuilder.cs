8dbba26efe8e115e04d07bb8b74910c3cfed4677
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-05 01:13:52 +0100

    Vishizhukel update

--- src/VishnetIntegrationTestToolsContainerBuilder.cs
+++ src/VishnetIntegrationTestToolsContainerBuilder.cs
@@ -1,12 +1,11 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
-using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Autofac;
 
 namespace Aspenlaub.Net.GitHub.CSharp.VishnetIntegrationTestTools;
 
 public static class VishnetIntegrationTestToolsContainerBuilder {
     public static ContainerBuilder RegisterForVishizhukelNetIntegrationTest(this ContainerBuilder builder, string applicationName) {
-        builder.UseDvinAndPegh(applicationName, new DummyCsArgumentPrompter());
+        builder.UseDvinAndPegh(applicationName);
         return builder;
     }
 }
